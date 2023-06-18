c9749efdfd8c51818d8b2cbcde93b1f2553b7c2f
Iranel González <iranelgonzalezv@gmail.com>
2023-06-18 14:18:32 +0200

    update

---
+++ Week03/Lab _SQL_Joins_on_multiple_tables_IDGV_week03.sql
@@ -0,0 +1,28 @@
+# Week 03
+# Lab | SQL Joins on multiple tables
+
+use sakila;
+
+#1. Write a query to display for each store its store ID, city, and country.
+
+
+#2. Write a query to display how much business, in dollars, each store brought in.
+
+
+#3. What is the average running time(length) of films by category?
+
+
+#4. Which film categories are longest(length) (find Top 5)? (Hint: You can rely on question 3 output.)
+
+
+#5. Display the top 5 most frequently(number of times) rented movies in descending order.
+
+
+#6. List the top five genres in gross revenue in descending order.
+
+
+#7. Is "Academy Dinosaur" available for rent from Store 1?
+
+
+
+
